1e0880d15efda1cf89b2a7af52d58b1dbe5983db
yuda3 <yuda3@hotmail.com>
2024-06-07 19:43:27 +0900

    MyBatis Refactoring

---
+++ src/main/java/com/example/repository/MyBatisUtil.java
@@ -0,0 +1,26 @@
+package com.example.repository;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
+
+public class MyBatisUtil {
+    private static SqlSessionFactory sqlSessionFactory;
+    static {
+        try {
+            String resource = "mybatis-config/config.xml";
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory =
+                    new SqlSessionFactoryBuilder().build(inputStream);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static SqlSession openSession(){
+        return sqlSessionFactory.openSession();
+    }
+}
