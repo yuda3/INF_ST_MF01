bc04a905ad59aae8ba77ac466b2c0c66bb0f048c
yuda3 <yuda3@hotmail.com>
2024-06-07 16:15:28 +0900

    Servlet Setting

---
+++ src/main/java/com/example/controller/HelloServlet.java
@@ -0,0 +1,20 @@
+package com.example.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//http://localhost:8081/MF01/hello
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("Hello Servlet");
+    }
+}
+
